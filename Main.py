416ebc9e8a66bfd27751c45723ede011c98dc7f7
tiondominic <145018221+tiondominic@users.noreply.github.com>
2026-01-21 11:15:01 +0000

    Added basic buttons

--- Main.py
+++ Main.py
@@ -1,4 +1,26 @@
-#TODO add UI
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Calculator")
+        self.setFixedSize(300, 300)
+
+        for i in range(9):
+            index = i + 1
+            btn = QPushButton(str(index), self)
+            btn.setFixedSize(QSize(100, 100))
+            btn.move((i % 3) * 100, (i // 3) * 100)
+            btn.clicked.connect(lambda checked=False, idx=index: self.onPress(idx))
+
+    def onPress(self, num):
+        print(num)
+
+
 
 def add(a, b):
     pass
@@ -23,6 +45,15 @@ def operation(op, a, b):
     elif op == '/':
         return divide(a, b)
     
+
+# main loop of program
+app = QApplication(sys.argv)
+
+window = Calculator()
+window.show()
+
+app.exec()
+
 
 ans = None
 while True:
